5c141b57fab81b7223e475b2bae77022315cb3e4
niketarachhadia <niketa.rachhadia@gmail.com>
2016-08-06 00:58:06 -0500

    Quiz app First Commit

---
+++ js/data.js
@@ -0,0 +1,54 @@
+// var q1 = "Q 1 - Which built-in method calls a function for each element in the array?";
+// var q2 = "Q 2 - Which of the following function of String object returns the characters in a string between two indexes into the string?";
+// var q3 = "Q 3 - Which of the following function of String object creates a string to blink as if it were in a <blink> tag?";
+// var q4 = "Q 4 - Which of the following function of Array object joins all elements of an array into a string?";
+// var q5 = "Q 5 - Which built-in method returns the character at the specified index?";
+// var q6 = "Q 6 - Which built-in method reverses the order of the elements of an array?";
+// var q7 = "Q 7 - Which of the following function of Array object sorts the elements of an array?";
+// var q8 = "Q 8 - Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?";
+// var q9 = "Q 9 - Which of the following function of Array object represents the source code of an object?";
+// var q10 = "Q 10 - Which of the following is the correct syntax to print a page using JavaScript?";
+
+
+var Quiz = [{
+	"question" : "Q 1 - Which built-in method calls a function for each element in the array?",
+	"choices" : [ "while()","loop()","forEach()","None of above"],
+	"correct_answer" : "forEach()"
+},{"question" : "Q 2 - Which of the following function of String object returns the characters in a string between two indexes into the string?",
+	"choices" : ["slice()","splice()","substr()","substring()"],
+	"correct_answer" : "substring()"
+
+},{"question" : "Q 3 - Which of the following function of String object creates a string to blink as if it were in a <blink> tag?",
+	"choices" : ["anchor()","big()","blink()","italics()"],
+	"correct_answer" : "blink()"
+
+},{"question" : "Q 4 - Which of the following function of Array object joins all elements of an array into a string?",
+	"choices" : ["concat()","join()","pop()","map()"],
+	"correct_answer" : "join()"
+
+},{"question" : "Q 5 - Which built-in method returns the character at the specified index?",
+	"choices" : ["characterAt()","getCharAt()","charAt()","None of the above"],
+	"correct_answer" : "charAt()"
+
+},{"question" : "Q 6 - Which built-in method reverses the order of the elements of an array?",
+	"choices" : ["changeOrder(order)","reverse()","sort(order)","None of the above."],
+	"correct_answer" : "reverse()"
+
+},{"question" : "Q 7 - Which of the following function of Array object sorts the elements of an array?",
+	"choices" : ["toSource()","sort()","toString()","unshift()"],
+	"correct_answer" : "sort()"
+
+},{"question" : "Q 8 - Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?",
+	"choices" : ["anchor()","big()","blink()","bold()"],
+	"correct_answer" : "bold()"
+
+},{"question" : "Q 9 - Which of the following function of Array object represents the source code of an object?",
+	"choices" : ["toSource()","splice()","toString()","unshift()"],
+	"correct_answer" : "toSourse()"
+
+},{"question" : "Q 10 - Which of the following is the correct syntax to print a page using JavaScript?",
+	"choices" : ["window.print();","B - browser.print();","navigator.print();","document.print();"],
+	"correct_answer" : "window.print();"
+
+}];
+
